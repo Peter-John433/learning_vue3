c8cf4e8c16669eb0f075b8d843e95aec258aa499
iampeterjohn <peterjeche442@gmail.com>
2024-03-20 13:27:43 -1100

    learning vue

---
+++ src/store/product.js
@@ -0,0 +1,20 @@
+// import { defineStore } from 'pinia';
+// // import { ref, computed } from 'vue';
+
+// export const useUserStore = defineStore('user', {
+//     state: () => {
+//         return {
+//             count: 10
+//         }
+//     },
+
+//     getters: {
+//         worker(state) { 
+//             return state.count * 2
+//         },
+
+//         myWork(worker){
+//             return this.worker + 1
+//         }
+//     }
+// }) 
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        vue learning project
+    </div>
+</template>
